7b7d1e7c7415c4244b0106bfa5ea3a38eeab7a89
yaseeen96 <syedyaseen1539@gmail.com>
2025-04-30 02:33:07 +0530

    update model and dummy data

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,19 +1,79 @@
 import 'package:shaheen_home/website_model.dart';
 
-const List<LinkItem> dummyItems = [
-  LinkItem(name: 'Google',        url: 'https://google.com',        logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Flutter',       url: 'https://flutter.dev',       logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Dart',          url: 'https://dart.dev',          logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'GitHub',        url: 'https://github.com',        logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'StackOverflow', url: 'https://stackoverflow.com', logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'YouTube',       url: 'https://youtube.com',       logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Medium',        url: 'https://medium.com',        logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Facebook',      url: 'https://facebook.com',      logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Twitter',       url: 'https://twitter.com',       logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Instagram',     url: 'https://instagram.com',     logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'LinkedIn',      url: 'https://linkedin.com',      logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Reddit',        url: 'https://reddit.com',        logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Amazon',        url: 'https://amazon.com',        logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Pinterest',     url: 'https://pinterest.com',     logoAsset: 'assets/images/google.png'),
-  LinkItem(name: 'Slack',         url: 'https://slack.com',         logoAsset: 'assets/images/google.png'),
+List<LinkItem> dummyItems = [
+  LinkItem(
+      docId: '1',
+      name: 'Google',
+      url: 'https://google.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '2',
+      name: 'Flutter',
+      url: 'https://flutter.dev',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '3',
+      name: 'Dart',
+      url: 'https://dart.dev',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '4',
+      name: 'GitHub',
+      url: 'https://github.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '5',
+      name: 'StackOverflow',
+      url: 'https://stackoverflow.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '6',
+      name: 'YouTube',
+      url: 'https://youtube.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '7',
+      name: 'Medium',
+      url: 'https://medium.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '8',
+      name: 'Facebook',
+      url: 'https://facebook.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '9',
+      name: 'Twitter',
+      url: 'https://twitter.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '10',
+      name: 'Instagram',
+      url: 'https://instagram.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '11',
+      name: 'LinkedIn',
+      url: 'https://linkedin.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '12',
+      name: 'Reddit',
+      url: 'https://reddit.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '13',
+      name: 'Amazon',
+      url: 'https://amazon.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '14',
+      name: 'Pinterest',
+      url: 'https://pinterest.com',
+      icon: 'assets/images/google.png'),
+  LinkItem(
+      docId: '15',
+      name: 'Slack',
+      url: 'https://slack.com',
+      icon: 'assets/images/google.png'),
 ];
